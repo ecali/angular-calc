31a908ce48dfe975c228fc4cc6d3729ef4f8e8b5
ecali <78408765+ecali@users.noreply.github.com>
2024-12-26 13:10:06 +0100

    handle variant for buttons && minor on template

--- src/app/shared/ui/calculator-button/calculator-button.component.ts
+++ src/app/shared/ui/calculator-button/calculator-button.component.ts
@@ -4,10 +4,16 @@ import {Component, Input} from '@angular/core';
   selector: 'app-calculator-button',
   imports: [],
   template: `
-    <button class="w-full px-4 py2 rounded bg-gray-700 text-gray-100">{{label}}</button>
+    <button [class]="getStyle()">{{label}}</button>
   `,
   styles: ``
 })
 export class CalculatorButtonComponent {
   @Input({required: true}) label: string = '';
+  @Input({required: false}) variant: 'gray' | 'yellow' = 'gray';
+
+  getStyle = () => {
+    return 'w-full px-6 py-4 rounded bg-gray-700 text-gray-100'.concat(' ', this.variant === 'gray' ? 'bg-gray-700' : 'bg-yellow-600')
+  }
+
 }
