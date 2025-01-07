b749e8055826d8ee8f7f0f34ee779e3fdfded3d0
ecali <78408765+ecali@users.noreply.github.com>
2025-01-07 16:34:35 +0100

    update test for ui component

--- src/app/shared/ui/calculator-button/calculator-button.component.spec.ts
+++ src/app/shared/ui/calculator-button/calculator-button.component.spec.ts
@@ -1,23 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CalculatorButtonComponent } from './calculator-button.component';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('CalculatorButtonComponent', () => {
   let component: CalculatorButtonComponent;
   let fixture: ComponentFixture<CalculatorButtonComponent>;
+  let buttonElement: DebugElement;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CalculatorButtonComponent]
-    })
-    .compileComponents();
-
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [CalculatorButtonComponent],
+    });
     fixture = TestBed.createComponent(CalculatorButtonComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    buttonElement = fixture.debugElement.query(By.css('button'));
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the correct label', () => {
+    component.label = '1';
+    fixture.detectChanges();
+    expect(buttonElement.nativeElement.textContent).toBe('1');
+  });
+
+  it('should apply the correct style for gray variant', () => {
+    component.variant = 'gray';
+    fixture.detectChanges();
+    expect(buttonElement.nativeElement.classList).toContain('bg-gray-700');
+    expect(buttonElement.nativeElement.classList).not.toContain('bg-yellow-600');
+  });
+
+  it('should apply the correct style for yellow variant', () => {
+    component.variant = 'yellow';
+    fixture.detectChanges();
+    expect(buttonElement.nativeElement.classList).toContain('bg-yellow-600');
+    expect(buttonElement.nativeElement.classList).not.toContain('bg-gray-700');
+  });
+
 });
--- src/app/shared/ui/calculator-button/calculator-button.component.ts
+++ src/app/shared/ui/calculator-button/calculator-button.component.ts
@@ -13,7 +13,7 @@ export class CalculatorButtonComponent {
   @Input({required: false}) variant: 'gray' | 'yellow' = 'gray';
 
   getStyle = () => {
-    return 'hover:scale-105 shadow-inner w-full drop-shadow-[0_10px_10px_rgba(255,255,255,0.15)] px-4 py-4 rounded-xl bg-gray-700 text-gray-100'.concat(' ', this.variant === 'gray' ? 'bg-gray-700' : 'bg-yellow-600')
+    return 'hover:scale-105 shadow-inner w-full drop-shadow-[0_10px_10px_rgba(255,255,255,0.15)] px-4 py-4 rounded-xl text-gray-100'.concat(' ', this.variant === 'gray' ? 'bg-gray-700' : 'bg-yellow-600')
   }
 
 }
