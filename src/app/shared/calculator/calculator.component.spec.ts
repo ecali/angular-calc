b7a0288aad42d4462b6098dc54370745d54679f1
ecali <78408765+ecali@users.noreply.github.com>
2025-01-07 16:34:23 +0100

    update test

--- src/app/shared/calculator/calculator.component.spec.ts
+++ src/app/shared/calculator/calculator.component.spec.ts
@@ -1,154 +1,102 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CalculatorComponent } from './calculator.component';
+import { CalculatorService } from '../../services/calculator.service';
+import { CalculatorButtonComponent } from '../ui/calculator-button/calculator-button.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
   let fixture: ComponentFixture<CalculatorComponent>;
+  let calculatorService: CalculatorService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CalculatorComponent]
-    })
-      .compileComponents();
+  beforeEach(() => {
+    const calculatorServiceMock = {
+      getDisplayNumber: jasmine.createSpy().and.returnValue('0'),
+      getHistory: jasmine.createSpy().and.returnValue([]),
+      clear: jasmine.createSpy(),
+      handleOperatorKey: jasmine.createSpy(),
+      calculate: jasmine.createSpy(),
+      handleCanc: jasmine.createSpy(),
+      handleNumber: jasmine.createSpy()
+    };
+
+    TestBed.configureTestingModule({
+      imports: [CalculatorComponent, CalculatorButtonComponent],
+      providers: [
+        { provide: CalculatorService, useValue: calculatorServiceMock }
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CalculatorComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    calculatorService = TestBed.inject(CalculatorService);
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should clear all values', () => {
-    component.clear();
-    expect(component.displayNumber).toBe('0');
-    expect(component.firstOperand).toBeNull();
-    expect(component.secondOperand).toBeNull();
-    expect(component.operator).toBeNull();
-    expect(component.history).toEqual([]);
-    expect(component.calculated).toBeFalse();
+
+  it('should call handleNumber when a number key is pressed', () => {
+    const key = '5';
+    component.pressKey(key);
+    expect(calculatorService.handleNumber).toHaveBeenCalledWith(key);
   });
 
-  it('should handle number key press', () => {
-    component.pressKey('5');
-    expect(component.displayNumber).toBe('5');
-    expect(component.firstOperand).toBe(5);
-    component.pressKey('3');
-    expect(component.displayNumber).toBe('53');
-    expect(component.firstOperand).toBe(53);
+  it('should call handleOperatorKey when an operator key is pressed', () => {
+    const key = '+';
+    component.pressKey(key);
+    expect(calculatorService.handleOperatorKey).toHaveBeenCalledWith(key);
   });
 
-  it('should handle operator key press', () => {
-    component.pressKey('5');
-    component.pressKey('+');
-    expect(component.operator).toBe('+');
-    expect(component.firstOperand).toBe(5);
-    expect(component.history).toEqual(['5', '+']);
+  it('should call calculate when "=" key is pressed', () => {
+    const key = '=';
+    component.pressKey(key);
+    expect(calculatorService.calculate).toHaveBeenCalled();
   });
 
-  it('should handle equals key press and calculate result', () => {
-    component.pressKey('5');
-    component.pressKey('+');
-    component.pressKey('3');
-    component.pressKey('=');
-    expect(component.displayNumber).toBe('8');
-    expect(component.history).toEqual(['5', '+', '3', '=']);
+  it('should call handleCanc when "c" key is pressed', () => {
+    const key = 'c';
+    component.pressKey(key);
+    expect(calculatorService.handleCanc).toHaveBeenCalled();
   });
 
-  it('should handle multiple operations', () => {
-    component.pressKey('5');
-    component.pressKey('+');
-    component.pressKey('3');
-    component.pressKey('=');
-    component.pressKey('*');
-    component.pressKey('2');
-    component.pressKey('=');
-    expect(component.displayNumber).toBe('16');
-    expect(component.history).toEqual(['5', '+', '3', '=', '8', '*', '2', '=']);
+  it('should call clear when "AC" key is pressed', () => {
+    const key = 'AC';
+    component.pressKey(key);
+    expect(calculatorService.clear).toHaveBeenCalled();
   });
 
-  it('should handle division by zero', () => {
-    component.pressKey('5');
-    component.pressKey('/');
-    component.pressKey('0');
-    component.pressKey('=');
-    expect(component.displayNumber).toBe('Infinity');
+  it('should call handleKeyboardEvent when a key is pressed', () => {
+    const event = new KeyboardEvent('keydown', { key: '1' });
+    spyOn(component, 'pressKey');
+    component.handleKeyboardEvent(event);
+    expect(component.pressKey).toHaveBeenCalledWith('1');
   });
 
-  it('should handle decimal numbers', () => {
-    component.pressKey('5');
-    component.pressKey('.');
-    component.pressKey('5');
-    component.pressKey('+');
-    component.pressKey('2');
-    component.pressKey('.');
-    component.pressKey('5');
-    component.pressKey('=');
-    expect(component.displayNumber).toBe('8');
+  it('should handle keyboard events correctly for operators', () => {
+    const event = new KeyboardEvent('keydown', { key: '+' });
+    spyOn(component, 'pressKey');
+    component.handleKeyboardEvent(event);
+    expect(component.pressKey).toHaveBeenCalledWith('+');
   });
 
-  it('should not calculate when insufficient operands are provided', () => {
-    component.pressKey('+');
-    component.pressKey('=');
-    expect(component.displayNumber).toBe('0');
+  it('should prevent default action on keydown event', () => {
+    const event = new KeyboardEvent('keydown', { key: '1' });
+    spyOn(event, 'preventDefault');
+    component.handleKeyboardEvent(event);
+    expect(event.preventDefault).toHaveBeenCalled();
   });
 
-  it('should clear all values when clear key is pressed', () => {
-    component.pressKey('9');
-    component.pressKey('+');
-    component.pressKey('3');
-    component.clear();
-    expect(component.displayNumber).toBe('0');
-    expect(component.firstOperand).toBeNull();
-    expect(component.secondOperand).toBeNull();
-    expect(component.operator).toBeNull();
-    expect(component.history).toEqual([]);
-    expect(component.calculated).toBeFalse();
+  it('should call pressKey with correct argument for Enter and Backspace keys', () => {
+    const eventEnter = new KeyboardEvent('keydown', { key: 'Enter' });
+    spyOn(component, 'pressKey');
+    component.handleKeyboardEvent(eventEnter);
+    expect(component.pressKey).toHaveBeenCalledWith('=');
+
+    const eventBackspace = new KeyboardEvent('keydown', { key: 'Backspace' });
+    component.handleKeyboardEvent(eventBackspace);
+    expect(component.pressKey).toHaveBeenCalledWith('c');
   });
-
-  it('should not allow the first operand to exceed 15 digits', () => {
-    for (let i = 0; i < 20; i++) {
-      component.pressKey('1');
-    }
-    expect(component.displayNumber.length).toBeLessThanOrEqual(15);
-    expect(component.displayNumber).toBe('111111111111111');
-    expect(component.firstOperand).toBe(111111111111111);
-  });
-
-  it('should not allow the second operand to exceed 15 digits', () => {
-    component.pressKey('1');
-    component.pressKey('+');
-    for (let i = 0; i < 20; i++) {
-      component.pressKey('2');
-    }
-    expect(component.displayNumber.length).toBeLessThanOrEqual(15);
-    expect(component.displayNumber).toBe('222222222222222');
-    expect(component.secondOperand).toBe(222222222222222);
-  });
-
-  it('should delete the last character of the current number when "C" is pressed', () => {
-    component.pressKey('1');
-    component.pressKey('2');
-    component.pressKey('3');
-    expect(component.displayNumber).toBe('123');
-    component.pressKey('c');
-    expect(component.displayNumber).toBe('12');
-    component.pressKey('c');
-    expect(component.displayNumber).toBe('1');
-    component.pressKey('c');
-    expect(component.displayNumber).toBe('0');
-    component.pressKey('1');
-    component.pressKey('+');
-    component.pressKey('4');
-    component.pressKey('5');
-    expect(component.displayNumber).toBe('45');
-    component.pressKey('c');
-    expect(component.displayNumber).toBe('4');
-    component.pressKey('c');
-    expect(component.displayNumber).toBe('0');
-  });
-
-
 });
